2ec0dd141f8d314fa1799402b1435c00c84f56ca
Giltwizy <giltonbosco@yahoo.com>
2020-05-26 03:42:33 +0300

    implemented using object oriented paradigm

--- beautifulsoup.py
+++ beautifulsoup.py
@@ -1,17 +1,33 @@
+###############################################
+# program: scrap-corona.py
+# author: Gilton Bosco
+# version: 1.1
+# date: 25 May 2020
+###############################################
+
+# requests module handles http requests
+# BeautifulSoup parses the html data
+
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://www.worldometers.info/coronavirus/"
-our_site = requests.get(url)
-try:
-    our_site.raise_for_status()
-except Exception as e:
-    print('There was a problem: %s'%(e))
+class Corona_updates:
+    
+    def __init__(self,url):
+        self.url = url       
+    
+    def get_data(self):
+        our_site = requests.get(self.url)
+        try:
+            our_site.raise_for_status()
+        except Exception as e:
+            print('There was a problem: %s'%(e))
+        s = BeautifulSoup(our_site.text,"html.parser")
+        data  = s.find_all("div",class_ = "maincounter-number")
+        print("Total Cases: ",data[0].text)
+        print("Total Deaths: ",data[1].text)
+        print("Total Recoveries: ",data[2].text)
 
-s = BeautifulSoup(our_site.text,"html.parser")
-data  = s.find_all("div",class_ = "maincounter-number")
+corona_data = Corona_updates("https://www.worldometers.info/coronavirus/")
+corona_data.get_data()
 
-
-print("Total Cases: ",data[0].text)
-print("Total Deaths: ",data[1].text)
-print("Total Recoveries: ",data[2].text)
