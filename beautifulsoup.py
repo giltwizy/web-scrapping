2f58839755f4d94ee5749faaa2d922aada261dcf
Giltwizy <giltonbosco@yahoo.com>
2020-05-26 03:55:17 +0300

    added comments

--- beautifulsoup.py
+++ beautifulsoup.py
@@ -1,5 +1,5 @@
 ###############################################
-# program: scrap-corona.py
+# program: corona-scrapper.py
 # author: Gilton Bosco
 # version: 1.1
 # date: 25 May 2020
@@ -14,16 +14,23 @@ from bs4 import BeautifulSoup
 class Corona_updates:
     
     def __init__(self,url):
+        '''initialize the class with the url'''
         self.url = url       
     
     def get_data(self):
+        '''Makes an http request and download the html data'''
         our_site = requests.get(self.url)
+
+        #get exception if occurs when failed to download the html content
         try:
             our_site.raise_for_status()
         except Exception as e:
             print('There was a problem: %s'%(e))
         s = BeautifulSoup(our_site.text,"html.parser")
+
+        #accessing all tags containing div ="maincounter-number" from the html
         data  = s.find_all("div",class_ = "maincounter-number")
+
         print("Total Cases: ",data[0].text)
         print("Total Deaths: ",data[1].text)
         print("Total Recoveries: ",data[2].text)
